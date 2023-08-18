91d4b9950b06163e5a1a2965395304b8b04d2981
Ahmedhassan139 <ahmedmagdy139@ymail.com>
2023-08-18 11:53:45 -0700

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *more_numbers - kmdl,
+ *return:llfvlkfl;,k
+ */
+void more_numbers(void)
+{
+	char i;
+
+	char j;
+	
+	for (i = '0'; i < 10; i++)
+	{
+		for (j = '0'; j <= 14; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
