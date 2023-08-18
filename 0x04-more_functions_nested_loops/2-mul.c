76a49a500c58e5cf39519a22657ca442c7428dcf
Ahmedhassan139 <ahmedmagdy139@ymail.com>
2023-08-18 07:54:02 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ *mul - return multiplication result
+ *@a: first number
+ *@b: second num
+ *Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,4 @@ void print_triangle(int size);
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
