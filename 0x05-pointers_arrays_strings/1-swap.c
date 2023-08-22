74c43d9e23526892376b0178da248c0512ab15e6
Ahmedhassan139 <ahmedmagdy139@ymail.com>
2023-08-22 14:21:34 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,20 +1,8 @@
-#include <stdio.h>
-/**
- *main - jn,ds.
- *Return: ekffl
- *@a:kfvlkf
- *@b:jkdlvlf
- */
-
-int main(void)
+void swap_int(int *a, int *b)
 {
-	int a;
-	int b;
+	int c;
 
-	a = 98;
-	b = 42;
-	printf("a=%d, b=%d\n", a, b);
-	swap_int(&a, &b);
-	printf("a=%d, b=%d\n", a, b);
-	return (0);
+	c = *a;
+	*a = *b;
+	*b = c;
 }
