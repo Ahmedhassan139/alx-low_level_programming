b267f5f964ea1d315948d46826165eab1872929c
Ahmedhassan139 <ahmedmagdy139@ymail.com>
2023-08-23 01:31:00 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+/**
+ **_strcat - jnkllkmckll
+ *return: jsndkvmf
+ *@dest:jnkvld
+ *@src:lfdvlf
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	return (dest);
+}
