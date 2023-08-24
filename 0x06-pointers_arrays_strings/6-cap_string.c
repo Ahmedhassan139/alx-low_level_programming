486bb8bf4a37df84e54eb56d191315bf48d9c3c9
Ahmedhassan139 <ahmedmagdy139@ymail.com>
2023-08-24 09:11:47 -0700

    ghp_Nw4cK

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+char *cap_string(char *s)
+{
+   int c = 0;
+   int i = 0; 
+
+   while (s[c] != '\0')
+   {
+     if ((i %2 == 1) && s[c] >= 'a' && s[c] <= 'z')
+     {
+        s[c] = s[c] - 32; 
+     }
+     else if(s[c] == ' ')
+     {
+        i++; 
+     }
+     c++;
+  }
+   return (s);
+}
