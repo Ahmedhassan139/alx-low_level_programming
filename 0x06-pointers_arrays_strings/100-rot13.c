0a52bd4fd0723d3872a07c897f273b2d5dd07c8c
Ahmedhassan139 <ahmedmagdy139@ymail.com>
2023-08-26 01:03:47 -0700

    100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,32 +3,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
+/**
+ *rot13 -jndm
+ *@src:lcffvf
+ *Return:mfmv,f;v.
 */
 
 char *rot13(const char *src)
-{char* current_char;
-	char* result;
-    result = malloc(strlen(src));
-    
-    if(result != NULL){
-      strcpy(result, src);
+{
+	char *current_char;
 
+	char *result;
 
-      current_char = result;
-      
-      while(*current_char != '\0'){
-        if((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90)){
-          if(*current_char > 109 || (*current_char > 77 && *current_char < 91)){
-  
-            *current_char -= 13;
-          }else{
-  
-            *current_char += 13;
-          }
-        }
-        current_char++;
-      }
-    }
-    return result;
+	result = malloc(strlen(src));
+
+	if (result != NULL)
+	{
+		strcpy(result, src);
+
+		current_char = result;
+
+		while (*current_char != '\0')
+		{
+			if ((*current_char >= 97 && *current_char <= 122) ||
+			    (*current_char >= 65 && *current_char <= 90))
+			{
+				if (*current_char > 109 || (*current_char > 77 && *current_char < 91))
+				{
+					*current_char -= 13;
+
+				}
+				else
+				{
+					*current_char += 13;
+				}
+			}
+			current_char++;
+		}
+	}
+	return (result);
 }
