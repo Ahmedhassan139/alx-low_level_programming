bc5b22c9121b7aad6887e7d5ef06e4b7db6e4bcb
Ahmedhassan139 <ahmedmagdy139@ymail.com>
2023-08-26 00:44:24 -0700

    100-rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+*/
+
+char *rot13(const char *src)
+{char* current_char;
+	char* result;
+    result = malloc(strlen(src));
+    
+    if(result != NULL){
+      strcpy(result, src);
+
+
+      current_char = result;
+      
+      while(*current_char != '\0'){
+        if((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90)){
+          if(*current_char > 109 || (*current_char > 77 && *current_char < 91)){
+  
+            *current_char -= 13;
+          }else{
+  
+            *current_char += 13;
+          }
+        }
+        current_char++;
+      }
+    }
+    return result;
+}
