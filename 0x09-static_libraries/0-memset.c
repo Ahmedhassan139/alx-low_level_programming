4b4675d419fc3af886d20719ab8faef5dcb11a16
Ahmed Hassan <ahmedmagdy139@ymail.com>
2023-10-03 20:10:31 +0300

    jskhgs

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,16 @@
+/**
+ * _memset - fiy with a constant byte
+ * @s: pointer to memory area 's'
+ * @b: constant bytto fill
+ * @n: number of bytes to fill
+ * Return: pointer ta 's'
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
